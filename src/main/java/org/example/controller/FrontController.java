361fd337f77e1b3c4f94fea08b83f919103260fd
André Marcondes Teixeira <andre.teixeira@dock.tech>
2025-03-18 14:28:15 -0300

    [WIP] Refatorando FrontController para encaminhar os comandos para seus respectivos controllers

--- src/main/java/org/example/controller/FrontController.java
+++ src/main/java/org/example/controller/FrontController.java
@@ -1,9 +1,15 @@
 package org.example.controller;
 
 public class FrontController {
+    private final CartoesController cartoesController;
+    private final ClienteController clienteController;
+    private final ContasController contasController;
     private final FaturasController faturasController;
 
     public FrontController() {
+        this.cartoesController = new CartoesController();
+        this.clienteController = new ClienteController();
+        this.contasController = new ContasController();
         this.faturasController = new FaturasController();
     }
 
@@ -21,6 +27,22 @@ public class FrontController {
         }
 
         var inicioComando = comando.split(" ")[0];
+
+        switch (inicioComando) {
+
+        }
+
+        if ("cartoes".equals(inicioComando)) {
+            return this.cartoesController.executar(comando);
+        }
+
+        if ("clientes".equals(inicioComando)) {
+            return this.clienteController.executar(comando);
+        }
+
+        if ("contas".equals(inicioComando)) {
+            return this.contasController.executar(comando);
+        }
 
         if ("faturas".equals(inicioComando)) {
             return this.faturasController.executar(comando);
