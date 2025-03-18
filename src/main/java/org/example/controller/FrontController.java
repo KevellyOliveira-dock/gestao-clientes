609a3cdd028b96cd94a74208eaef94437880c40c
André Marcondes Teixeira <andre.teixeira@dock.tech>
2025-03-18 14:18:09 -0300

    Refatorado código em FrontController para invocar FaturasController sempre que o comando iniciar com "faturas"

--- src/main/java/org/example/controller/FrontController.java
+++ src/main/java/org/example/controller/FrontController.java
@@ -1,102 +1,16 @@
 package org.example.controller;
 
 public class FrontController {
+    private final FaturasController faturasController;
+
+    public FrontController() {
+        this.faturasController = new FaturasController();
+    }
 
     public String executar(String comando) {
+        comando = comando.trim();
 
-        if (!comando.isEmpty()) {
-            switch (comando) {
-                case "cartoes":
-                    return """
-                            -------------------------------
-                            | Bloquear {número do cartao} |
-                            | Cadastrar                   |
-                            -------------------------------""";
-
-                case "cartoes bloquear":
-                    return "não implementado";
-
-                case "cartoes cadastrar":
-                    return "não implementado";
-
-                case "clientes":
-                    return """
-                            ------------------------------
-                            | atualizar {cpf do cliente} |
-                            | cadastrar                  |
-                            | desativar {cpf do cliente} |
-                            | pesquisar                  |
-                            ------------------------------""";
-
-                case "clientes atualizar":
-                    return "não implementado";
-
-                case "clientes cadastrar":
-                    return "não implementado";
-
-                case "clientes desativar":
-                    return "não implementado";
-
-                case "clientes pesquisar":
-                    return """
-                            --------------------------
-                            | cpf {cpf do cliente}   |
-                            | nome {nome do cliente} |
-                            -------------------------""";
-
-                case "clientes pesquisar cpf":
-                    return "não implementado";
-
-                case "clientes pesquisar nome":
-                    return "não implementado";
-
-                case "contas":
-                    return """
-                            -------------------------------
-                            | cadastrar                   |
-                            | desativar {numero da conta} |
-                            | extrato {numero da conta}   |
-                            | pesquisar                   |
-                            -------------------------------""";
-
-                case "contas cadastrar":
-                    return "não implementado";
-
-                case "contas desativar":
-                    return "não implementado";
-
-                case "contas extrato":
-                    return "não implementado";
-
-                case "contas pesquisar":
-                    return """
-                            ----------------------------------
-                            | cpf-titular {cpf do cliente}   |
-                            | nome-titular {nome do cliente} |
-                            | numero {número da conta}       |
-                            ----------------------------------""";
-
-                case "contas pesquisar cpf-titular":
-                    return "não implementado";
-
-                case "contas pesquisar nome-titular":
-                    return "não implementado";
-
-                case "contas pesquisar numero":
-                    return "não implementado";
-
-                case "faturas":
-                    FaturasController faturas = new FaturasController();
-                    return faturas.executar(comando);
-
-                case "faturas fechar":
-                    FaturasController faturasFechar = new FaturasController();
-                    return faturasFechar.executar(comando);
-
-                default:
-                    return "operação inválida";
-            }
-        } else {
+        if (comando.isEmpty()) {
             return """
                     ------------
                     | cartoes  |
@@ -104,6 +18,96 @@ public class FrontController {
                     | contas   |
                     | faturas  |
                     ------------""";
+        }
+
+        var inicioComando = comando.split(" ")[0];
+
+        if ("faturas".equals(inicioComando)) {
+            return this.faturasController.executar(comando);
+        }
+
+        switch (comando) {
+            case "cartoes":
+                return """
+                        -------------------------------
+                        | Bloquear {número do cartao} |
+                        | Cadastrar                   |
+                        -------------------------------""";
+
+            case "cartoes bloquear":
+                return "não implementado";
+
+            case "cartoes cadastrar":
+                return "não implementado";
+
+            case "clientes":
+                return """
+                        ------------------------------
+                        | atualizar {cpf do cliente} |
+                        | cadastrar                  |
+                        | desativar {cpf do cliente} |
+                        | pesquisar                  |
+                        ------------------------------""";
+
+            case "clientes atualizar":
+                return "não implementado";
+
+            case "clientes cadastrar":
+                return "não implementado";
+
+            case "clientes desativar":
+                return "não implementado";
+
+            case "clientes pesquisar":
+                return """
+                        --------------------------
+                        | cpf {cpf do cliente}   |
+                        | nome {nome do cliente} |
+                        -------------------------""";
+
+            case "clientes pesquisar cpf":
+                return "não implementado";
+
+            case "clientes pesquisar nome":
+                return "não implementado";
+
+            case "contas":
+                return """
+                        -------------------------------
+                        | cadastrar                   |
+                        | desativar {numero da conta} |
+                        | extrato {numero da conta}   |
+                        | pesquisar                   |
+                        -------------------------------""";
+
+            case "contas cadastrar":
+                return "não implementado";
+
+            case "contas desativar":
+                return "não implementado";
+
+            case "contas extrato":
+                return "não implementado";
+
+            case "contas pesquisar":
+                return """
+                        ----------------------------------
+                        | cpf-titular {cpf do cliente}   |
+                        | nome-titular {nome do cliente} |
+                        | numero {número da conta}       |
+                        ----------------------------------""";
+
+            case "contas pesquisar cpf-titular":
+                return "não implementado";
+
+            case "contas pesquisar nome-titular":
+                return "não implementado";
+
+            case "contas pesquisar numero":
+                return "não implementado";
+
+            default:
+                return "operação inválida";
         }
     }
 }
