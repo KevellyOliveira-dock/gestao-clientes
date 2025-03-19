2ae39b4cd1cc4e0aa9601970815b579c32fcb3e0
Kevelly <kevelly.oliveira@dock.tech>
2025-03-19 08:52:49 -0300

    movendo lógica de cartoes, clientes e contas do FrontController para CartoesController, ClienteController e ContasController

--- src/main/java/org/example/controller/CartoesController.java
+++ src/main/java/org/example/controller/CartoesController.java
@@ -1,5 +1,24 @@
 package org.example.controller;
 
 public class CartoesController {
-    
+
+    public String executar(String comando) {
+        switch (comando) {
+            case "cartoes":
+                return """
+                        -------------------------------
+                        | Bloquear {número do cartao} |
+                        | Cadastrar                   |
+                        -------------------------------""";
+
+            case "cartoes bloquear":
+                return "não implementado";
+
+            case "cartoes cadastrar":
+                return "não implementado";
+
+            default:
+                return "operação inválida";
+        }
+    }
 }
--- src/main/java/org/example/controller/FrontController.java
+++ src/main/java/org/example/controller/FrontController.java
@@ -28,10 +28,6 @@ public class FrontController {
 
         var inicioComando = comando.split(" ")[0];
 
-        switch (inicioComando) {
-
-        }
-
         if ("cartoes".equals(inicioComando)) {
             return this.cartoesController.executar(comando);
         }
@@ -48,88 +44,6 @@ public class FrontController {
             return this.faturasController.executar(comando);
         }
 
-        switch (comando) {
-            case "cartoes":
-                return """
-                        -------------------------------
-                        | Bloquear {número do cartao} |
-                        | Cadastrar                   |
-                        -------------------------------""";
-
-            case "cartoes bloquear":
-                return "não implementado";
-
-            case "cartoes cadastrar":
-                return "não implementado";
-
-            case "clientes":
-                return """
-                        ------------------------------
-                        | atualizar {cpf do cliente} |
-                        | cadastrar                  |
-                        | desativar {cpf do cliente} |
-                        | pesquisar                  |
-                        ------------------------------""";
-
-            case "clientes atualizar":
-                return "não implementado";
-
-            case "clientes cadastrar":
-                return "não implementado";
-
-            case "clientes desativar":
-                return "não implementado";
-
-            case "clientes pesquisar":
-                return """
-                        --------------------------
-                        | cpf {cpf do cliente}   |
-                        | nome {nome do cliente} |
-                        -------------------------""";
-
-            case "clientes pesquisar cpf":
-                return "não implementado";
-
-            case "clientes pesquisar nome":
-                return "não implementado";
-
-            case "contas":
-                return """
-                        -------------------------------
-                        | cadastrar                   |
-                        | desativar {numero da conta} |
-                        | extrato {numero da conta}   |
-                        | pesquisar                   |
-                        -------------------------------""";
-
-            case "contas cadastrar":
-                return "não implementado";
-
-            case "contas desativar":
-                return "não implementado";
-
-            case "contas extrato":
-                return "não implementado";
-
-            case "contas pesquisar":
-                return """
-                        ----------------------------------
-                        | cpf-titular {cpf do cliente}   |
-                        | nome-titular {nome do cliente} |
-                        | numero {número da conta}       |
-                        ----------------------------------""";
-
-            case "contas pesquisar cpf-titular":
-                return "não implementado";
-
-            case "contas pesquisar nome-titular":
-                return "não implementado";
-
-            case "contas pesquisar numero":
-                return "não implementado";
-
-            default:
-                return "operação inválida";
-        }
+        return "operação inválida";
     }
 }
