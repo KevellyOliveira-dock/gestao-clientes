87dc8b86e0556f53502b507d6dbcdb14357fa3b7
Kevelly <kevelly.oliveira@dock.tech>
2025-03-20 14:43:38 -0300

    Criando classe FakeController

--- src/test/java/org/example/controller/FrontControllerTest.java
+++ src/test/java/org/example/controller/FrontControllerTest.java
@@ -7,19 +7,39 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FrontControllerTest {
     private FrontController controller;
+    private Controller fakeCartoesController;
+    private Controller fakeClientesController;
+    private Controller fakeContasController;
+    private Controller fakeFaturasController;
 
     @BeforeEach
     public void setup() {
-        var cartoesController = new CartoesController();
-        var clientesController = new ClientesController();
-        var contasController = new ContasController();
-        var faturasController = new FaturasController();
+        this.fakeCartoesController = new FakeController();
+        this.fakeClientesController = new FakeController();
+        this.fakeContasController = new FakeController();
+        this.fakeFaturasController = new FakeController();
 
         controller = new FrontController(
-                cartoesController,
-                clientesController,
-                contasController,
-                faturasController
+                this.fakeCartoesController,
+                this.fakeClientesController,
+                this.fakeContasController,
+                this.fakeFaturasController
         );
+    }
+
+    @Test
+    public void QuandoComandoForCartoesEntaoChamarCartoesController() {
+        controller.executar("cartoes aaaa");
+        assertEquals("cartoes aaaa", (FakeController) this.fakeCartoesController.comando);
+    }
+
+    class FakeController implements Controller {
+        public String comando;
+
+        @Override
+        public String executar(String comando) {
+            this.comando = comando;
+            return "";
+        }
     }
 }
