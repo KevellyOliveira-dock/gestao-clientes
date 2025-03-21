f163cb9fd374d905e05cc03adc67e1bba1208761
Kevelly <kevelly.oliveira@dock.tech>
2025-03-21 12:03:44 -0300

    Verificando se a FrontController está direcionando cada comando para a sua respectiva Controller

--- src/test/java/org/example/controller/FrontControllerTest.java
+++ src/test/java/org/example/controller/FrontControllerTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FrontControllerTest {
+
+    //declaração das variáveis e seus tipos, usadas para testes
     private FrontController controller;
     private Controller fakeCartoesController;
     private Controller fakeClientesController;
@@ -14,11 +16,13 @@ public class FrontControllerTest {
 
     @BeforeEach
     public void setup() {
+        //Criando instâncias fake de cada controller, usadas como "substitutas" para os reais
         this.fakeCartoesController = new FakeController();
         this.fakeClientesController = new FakeController();
         this.fakeContasController = new FakeController();
         this.fakeFaturasController = new FakeController();
 
+        //criando a instancia da FrontController, passando os controlers fakes
         controller = new FrontController(
                 this.fakeCartoesController,
                 this.fakeClientesController,
@@ -27,18 +31,50 @@ public class FrontControllerTest {
         );
     }
 
+    //Testes que verificam se a FrontController chama o respectivo Controller
     @Test
     public void QuandoComandoForCartoesEntaoChamarCartoesController() {
-        controller.executar("cartoes aaaa");
-        assertEquals("cartoes aaaa", (FakeController) this.fakeCartoesController.comando);
+        //executa o comando
+        controller.executar("cartoes");
+
+        /**Verifica se o comando foi direcionado para a controller correta(no caso fakeCartoesController)
+         faz um cast(conversão de tipo) de fakeCartoesController para o tipo FakeController e acessa
+         atributo comando**/
+        String comandoRecebido = ((FakeController) this.fakeCartoesController).comando;
+
+        //Verifica se o comando recebido é o esperado
+        assertEquals("cartoes", comandoRecebido);
     }
 
-    class FakeController implements Controller {
-        public String comando;
+    @Test
+    public void QuandoComandoForClientesEntaoChamarClientesController() {
+        controller.executar("clientes");
+        String comandoRecebido = ((FakeController) this.fakeClientesController).comando;
+        assertEquals("clientes", comandoRecebido);
+    }
 
+    @Test
+    public void QuandoComandoForContasEntaoChamarContasController() {
+        controller.executar("contas");
+        String comandoRecebido = ((FakeController) this.fakeContasController).comando;
+        assertEquals("contas", comandoRecebido);
+    }
+
+    @Test
+    public void QuandoComandoForFaturasEntaoChamarFaturasController() {
+        controller.executar("faturas");
+        String comandoRecebido = ((FakeController) this.fakeFaturasController).comando;
+        assertEquals("faturas", comandoRecebido);
+    }
+
+    //Simula o comportamento dos Controllers reais para o teste
+    class FakeController implements Controller {
+        public String comando; //Armazena o comando recebido
+
+        //implementação do metodo executar, armazena o comando recebido
         @Override
         public String executar(String comando) {
-            this.comando = comando;
+            this.comando = comando; //guarda o comando recebido
             return "";
         }
     }
