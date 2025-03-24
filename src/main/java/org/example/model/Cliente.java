2b0b11b4f94a96a3f2ac99e8b90ebaf7943a9fb7
Kevelly <kevelly.oliveira@dock.tech>
2025-03-24 13:42:57 -0300

    Criando o contrutor a partir da anotação "@AllArgsConstructor"

--- src/main/java/org/example/model/Cliente.java
+++ src/main/java/org/example/model/Cliente.java
@@ -1,12 +1,13 @@
 package org.example.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 //@Data -> getter e setter
-@RequiredArgsConstructor //gera o construtor
+@AllArgsConstructor //gera o construtor
 public class Cliente {
     private String nomeCompleto;
     private String cpf;
