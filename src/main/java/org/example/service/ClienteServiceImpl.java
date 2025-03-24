ea4b83f550706d18772c2e9ce636f9f1ebfce106
Kevelly <kevelly.oliveira@dock.tech>
2025-03-24 13:03:37 -0300

    Gerando lista de clientes e Sobrescrevendo métodos do cliente

---
+++ src/main/java/org/example/service/ClienteServiceImpl.java
@@ -0,0 +1,18 @@
+package org.example.service;
+
+import org.example.model.Cliente;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ClienteServiceImpl implements ClienteService {
+    Set<Cliente> listaClientes = new HashSet<>();
+
+    @Override
+    public void cadastrarCliente(String nomeCompleto, String cpf, String endereco) {
+        //Cira um novo cliente do tipo cliente com os dados fornecidos
+        Cliente novoCliente = new Cliente(nomeCompleto, cpf, endereco);
+        listaClientes.add(novoCliente);
+    }
+}
+
