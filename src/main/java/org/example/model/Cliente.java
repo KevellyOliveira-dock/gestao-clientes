96fcaeb2eed6e49f1609dd6cb3c42e112ac77efe
Kevelly <kevelly.oliveira@dock.tech>
2025-03-24 13:24:45 -0300

    adicionando anotações do lombok e removendo Gets e construtor

--- src/main/java/org/example/model/Cliente.java
+++ src/main/java/org/example/model/Cliente.java
@@ -1,26 +1,14 @@
 package org.example.model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+//@Data -> getter e setter
+@RequiredArgsConstructor //gera o construtor
 public class Cliente {
     private String nomeCompleto;
     private String cpf;
     private String endereco;
-
-    public Cliente(String nomeCompleto, String cpf, String endereco) {
-        this.nomeCompleto = nomeCompleto;
-        this.cpf = cpf;
-        this.endereco = endereco;
-    }
-
-    public String getNomeCompleto() {
-        return nomeCompleto;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
 }
