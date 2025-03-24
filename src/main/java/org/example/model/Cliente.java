3a8b10373aec7f1b931b8ffd71324791a5997d0e
Kevelly <kevelly.oliveira@dock.tech>
2025-03-24 13:00:26 -0300

    criando model do Cliente, atributos que representam os dados do cliente, o contrutor e métodos get

--- src/main/java/org/example/model/Cliente.java
+++ src/main/java/org/example/model/Cliente.java
@@ -1,4 +1,26 @@
 package org.example.model;
 
 public class Cliente {
+    private String nomeCompleto;
+    private String cpf;
+    private String endereco;
+
+    public Cliente(String nomeCompleto, String cpf, String endereco) {
+        this.nomeCompleto = nomeCompleto;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
 }
