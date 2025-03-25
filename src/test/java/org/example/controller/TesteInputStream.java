9349e2c8e5e80500ded3ae0b2392a6ebbb67898a
Kevelly <kevelly.oliveira@dock.tech>
2025-03-25 09:47:41 -0300

    Alterando lógica de leitura do read do inputStream

--- src/test/java/org/example/controller/TesteInputStream.java
+++ src/test/java/org/example/controller/TesteInputStream.java
@@ -16,15 +16,23 @@ public class TesteInputStream extends InputStream {
 
     @Override
     public int read() throws IOException {
+        //ao chegar no final da String deve retornar -1, para indicar o fim
         if (indiceMaisRecente >= input.length()) {
             return -1;
         }
 
-        var bytes = String.valueOf(this.input.charAt(indiceMaisRecente)).getBytes(StandardCharsets.UTF_8);
-        indiceMaisRecente++;
-        ByteBuffer buffer = ByteBuffer.allocate(Integer.BYTES);
-        buffer.put(bytes);
-        buffer.rewind();
-        return buffer.getInt();
+        //Pega o caractere atual da String e add mais um ao indice
+        char caractere =  input.charAt(indiceMaisRecente++);
+
+        //char sendo convertido diretamente em int (type casting explícito - maior para menor)
+        return (int) caractere;
+
+        //Não daria certo porque o inputStrem retorna valores de 0 a 255, transformar em bytes gera valores invalidos
+        //var bytes = String.valueOf(this.input.charAt(indiceMaisRecente)).getBytes(StandardCharsets.UTF_8);
+        //indiceMaisRecente++;
+        //ByteBuffer buffer = ByteBuffer.allocate(Integer.BYTES);
+        //buffer.put(bytes);
+        //buffer.rewind();
+        //return buffer.getInt();
     }
 }
