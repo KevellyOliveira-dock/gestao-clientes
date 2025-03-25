f3ac9ed6ac530bf93dc65a4972194242cfa901c6
Kevelly <kevelly.oliveira@dock.tech>
2025-03-25 12:11:17 -0300

    Criando método para atualizar cliente

--- src/main/java/org/example/controller/ClientesController.java
+++ src/main/java/org/example/controller/ClientesController.java
@@ -1,6 +1,6 @@
 package org.example.controller;
 
-import java.io.InputStream;
+import org.example.model.Cliente;
 import org.example.service.ClienteService;
 
 import java.util.Scanner;
@@ -29,10 +29,10 @@ public class ClientesController implements Controller {
                         ------------------------------""";
 
             case "clientes atualizar":
-                return "não implementado";
+                return atualizarCliente();
 
             case "clientes cadastrar":
-               return cadastrarCliente();
+                return cadastrarCliente();
 
             case "clientes desativar":
                 return "não implementado";
@@ -68,5 +68,33 @@ public class ClientesController implements Controller {
         clienteService.cadastrarCliente(nomeCompleto, cpf, endereco);
 
         return "Cliente cadastrado com sucesso";
+    }
+
+    public String atualizarCliente() {
+        System.out.println("Informe seu CPF: ");
+        String cpf = scanner.nextLine();
+
+        Cliente clienteExistente = clienteService.verificarCPF(cpf);
+
+        if (clienteExistente == null) {
+            return "CPF não cadastrado";
+        } else {
+            System.out.println("Nome: [" + clienteExistente.getNomeCompleto() + "]");
+            String nomeCompleto = scanner.nextLine();
+
+            if (nomeCompleto.isEmpty()) {
+                nomeCompleto = clienteExistente.getNomeCompleto();
+            }
+
+            System.out.println("Endereço: [" + clienteExistente.getEndereco() + "]");
+            String endereco = scanner.nextLine();
+            if (endereco.isEmpty()) {
+                endereco = clienteExistente.getEndereco();
+            }
+
+            clienteService.atualizarCliente(nomeCompleto, cpf, endereco);
+
+            return "Cliente atualizado com sucesso";
+        }
     }
 }
