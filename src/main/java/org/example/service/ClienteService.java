69266025f7e9b9894f71476e71ca93b80bfd8e2f
Kevelly <kevelly.oliveira@dock.tech>
2025-03-25 11:51:26 -0300

    Adicionando métodos de verificarCPF e atualizarCleinte

--- src/main/java/org/example/service/ClienteService.java
+++ src/main/java/org/example/service/ClienteService.java
@@ -1,5 +1,11 @@
 package org.example.service;
 
+import org.example.model.Cliente;
+
 public interface ClienteService {
     void cadastrarCliente(String nomeCompleto, String cpf, String endereco);
+
+    Cliente verificarCPF(String cpf);
+
+    void atualizarCliente(String nomeAtualizado, String cpf, String enderecoAtualizado);
 }
