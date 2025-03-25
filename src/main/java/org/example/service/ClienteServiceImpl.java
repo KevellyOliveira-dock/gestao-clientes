d161aa566af0aea120d2e9a75bc93274f08bbff5
Kevelly <kevelly.oliveira@dock.tech>
2025-03-25 12:10:14 -0300

    Implementando lógica de verificarCPF e atualizarCliente

--- src/main/java/org/example/service/ClienteServiceImpl.java
+++ src/main/java/org/example/service/ClienteServiceImpl.java
@@ -1,6 +1,7 @@
 package org.example.service;
 
 import org.example.model.Cliente;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,9 +10,30 @@ public class ClienteServiceImpl implements ClienteService {
 
     @Override
     public void cadastrarCliente(String nomeCompleto, String cpf, String endereco) {
-        //Cira um novo cliente do tipo cliente com os dados fornecidos
+        //Cria um novo cliente do tipo cliente com os dados fornecidos
         Cliente novoCliente = new Cliente(nomeCompleto, cpf, endereco);
         listaClientes.add(novoCliente);
+    }
+
+    @Override
+    //Retorna Cliente para assim poder acessar Getters e Setters
+    public Cliente verificarCPF(String cpf) {
+        for (Cliente cliente : listaClientes) {
+            if (cliente.getCpf().equals(cpf)){
+                return cliente;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void atualizarCliente(String nomeAtualizado, String cpf, String enderecoAtualizado) {
+        Cliente atualizarCliente = verificarCPF(cpf);
+
+        if (atualizarCliente != null) {
+            atualizarCliente.setNomeCompleto(nomeAtualizado);
+            atualizarCliente.setEndereco(enderecoAtualizado);
+        }
     }
 }
 
