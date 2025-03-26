f09a34c695a9b0bb24eb16ea88d57d8cdd7a0007
Kevelly <kevelly.oliveira@dock.tech>
2025-03-26 14:01:34 -0300

    Adicionando nova dependência de parametros

--- src/test/java/org/example/service/ClienteServiceImplTest.java
+++ src/test/java/org/example/service/ClienteServiceImplTest.java
@@ -1,12 +1,10 @@
 package org.example.service;
 
-import org.example.controller.ClientesController;
-import org.example.controller.TesteInputStream;
 import org.example.model.Cliente;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Scanner;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,46 +19,45 @@ public class ClienteServiceImplTest {
     @Test
     public void quandoCadastrarClienteVerifiqueSeOCpfJaFoiCadastradoEntaoCadastreComSucesso() {
         //cria um usuario para teste
-        var resultado = clienteServiceImpl.cadastrarCliente("Kevelly", "5689778", "Rua teste");
-        Cliente cliente = clienteServiceImpl.verificarCPF("5689778");
+        var resultado = clienteServiceImpl.cadastrarCliente(
+                "Kevelly",
+                "5689778",
+                "Rua teste");
+        Cliente cliente = clienteServiceImpl.buscarClientePorCPF("5689778");
 
         var resultadoEsperado = "Cliente cadastrado com sucesso";
 
         assertEquals(resultadoEsperado, resultado);
         assertNotNull(cliente);
         assertEquals("Kevelly", cliente.getNomeCompleto());
+        assertEquals("5689778", cliente.getCpf());
         assertEquals("Rua teste", cliente.getEndereco());
     }
 
-    @Test
-    public void quandoVerificarCpfForNulaEntaoNaoCadastra() {
-        var resultado = clienteServiceImpl.cadastrarCliente("Kevelly", null, "Rua teste");
-        Cliente cliente = clienteServiceImpl.verificarCPF(null);
+    @ParameterizedTest
+    @NullAndEmptySource //quando a função for executada passa null e depois vazia
+    public void quandoBuscarClientePorCpfForVaziaEntaoNaoCadastra(String cpf) {
+        var resultado = clienteServiceImpl.cadastrarCliente("Kevelly", cpf, "Rua teste");
+        Cliente cliente = clienteServiceImpl.buscarClientePorCPF(cpf);
 
         assertEquals("CPF não pode ser nulo ou vazio", resultado);
         assertNull(cliente);
     }
 
     @Test
-    public void quandoVerificarCpfForVaziaEntaoNaoCadastra() {
-        var resultado = clienteServiceImpl.cadastrarCliente("Kevelly", "", "Rua teste");
-        Cliente cliente = clienteServiceImpl.verificarCPF("");
-
-        assertEquals("CPF não pode ser nulo ou vazio", resultado);
-        assertNull(cliente);
-    }
-
-    @Test
-    public void quandoVerificarCpfEntaoVerifiqueSeCadastrouChaveAntes() {
+    public void quandoBuscarClientePorCpfEntaoVerifiqueSeCadastrouChaveAntes() {
         clienteServiceImpl.cadastrarCliente("Kevelly", "5689778", "Rua teste");
-        var resultado = clienteServiceImpl.cadastrarCliente("Kevelly", "5689778", "Rua teste");
+        var resultado = clienteServiceImpl.cadastrarCliente(
+                "Kevelly",
+                "5689778",
+                "Rua teste");
 
         assertEquals("CPF já cadastrado", resultado);
     }
 
     @Test
-    public void quandoVerificarCpfECpfNaoExistirEntaoRetorneNulo() {
-        Cliente cliente = clienteServiceImpl.verificarCPF("");
+    public void quandoBuscarClientePorCpfECpfNaoExistirEntaoRetorneNulo() {
+        Cliente cliente = clienteServiceImpl.buscarClientePorCPF("");
         assertNull(cliente);
     }
 }
