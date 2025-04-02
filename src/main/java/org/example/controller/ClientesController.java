7e6f03549e2f989cc35afd4bf09ddf029e4c07eb
Kevelly <kevelly.oliveira@dock.tech>
2025-04-02 09:21:04 -0300

    Implementando Pesquisar cliente pelo nome

--- src/main/java/org/example/controller/ClientesController.java
+++ src/main/java/org/example/controller/ClientesController.java
@@ -3,6 +3,7 @@ package org.example.controller;
 import org.example.model.Cliente;
 import org.example.service.ClienteService;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class ClientesController implements Controller {
@@ -96,10 +97,25 @@ public class ClientesController implements Controller {
                 return "não implementado";
 
             case "nome":
-                return "não implementado";
+                return pesquisarClientesPorNome(partes[3]);
 
             default:
                 return "operação inválida";
         }
+    }
+
+    public String pesquisarClientesPorNome(String nome) {
+        List<Cliente> clientes = clienteService.pesquisarClientePorNome(nome);
+
+        if (clientes.isEmpty()) {
+            return "Nenhum cliente com esse nome foi encontrado.";
+        }
+
+        //Sugestão do StringBuilder
+        String resultado = "Clientes encontrados: \n";
+        for (Cliente cliente : clientes) {
+            resultado += cliente.toString() + "\n";
+        }
+        return resultado;
     }
 }
--- src/main/java/org/example/service/ClienteService.java
+++ src/main/java/org/example/service/ClienteService.java
@@ -2,10 +2,14 @@ package org.example.service;
 
 import org.example.model.Cliente;
 
+import java.util.List;
+
 public interface ClienteService {
     String cadastrarCliente(String nomeCompleto, String cpf, String endereco);
 
     Cliente buscarClientePorCPF(String cpf);
 
     String atualizarCliente(String nomeCompleto, String cpf, String endereco);
+
+    List<Cliente> pesquisarClientePorNome(String nome);
 }
