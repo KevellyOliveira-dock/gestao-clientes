b4e272b544fb075c2fe4a1933f0f05921a66391f
Kevelly <kevelly.oliveira@dock.tech>
2025-05-08 08:37:32 -0300

    Criado: mock de hashMap para testes

--- src/test/java/org/example/service/CartaoServiceImplTest.java
+++ src/test/java/org/example/service/CartaoServiceImplTest.java
@@ -3,13 +3,15 @@ package org.example.service;
 import org.example.model.Cartao;
 import org.example.model.Cliente;
 import org.example.model.Conta;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -18,6 +20,7 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 public class CartaoServiceImplTest {
 
+    @InjectMocks
     private CartaoServiceImpl cartaoServiceImpl;
 
     @Mock
@@ -26,15 +29,33 @@ public class CartaoServiceImplTest {
     @Mock
     private ContaService contaService;
 
+    @Mock
+    private HashMap<String, Cartao> mockHashMapCartao;
+
     private static final String NOME_CLIENTE = "Kevelly";
     private static final String CPF_CLIENTE = "12345678900";
     private static final String ENDERECO_CLIENTE = "Rua dos testes, 56";
     private static final Double SALDO_CONTA = 123.43;
     private static final String NUMERO_CONTA = "0";
+    private static final boolean IS_ATIVO_CONTA = true;
+    private static final String NUMERO_CARTAO = "1234";
+    private static final String CVV_CARTAO = "123";
+    private static final String DT_VENCIMENTO_CARTAO = "12/12/2028";
+    private static final boolean IS_BLOQUEADO_CARTAO = false;
 
-    @BeforeEach
-    public void setup() {
-        cartaoServiceImpl = new CartaoServiceImpl(clienteService, contaService);
+    @Test
+    public void quandoComandoForCadastrarCartaoVerifiqueSeOCpfEAContaFoiCadastradoEntaoCadastreComSucesso()
+            throws Exception {
+        Cliente cliente = new Cliente(NOME_CLIENTE, CPF_CLIENTE, ENDERECO_CLIENTE);
+        Conta conta = new Conta(NUMERO_CONTA, cliente, SALDO_CONTA, IS_ATIVO_CONTA);
+
+        when(clienteService.buscarClientePorCPF(CPF_CLIENTE)).thenReturn(cliente);
+        when(contaService.buscarContaPorNumero(NUMERO_CONTA)).thenReturn(conta);
+
+        Cartao resultadoReal = cartaoServiceImpl.cadastrarCartao(CPF_CLIENTE, NUMERO_CONTA);
+
+        assertEquals(cliente, resultadoReal.getCliente());
+        assertEquals(conta, resultadoReal.getConta());
     }
 
     @Test
@@ -96,6 +117,21 @@ public class CartaoServiceImplTest {
         assertEquals("A conta informada não está ativa.\n", exception.getMessage());
     }
 
+    @Test
+    public void quandoCartaoPesquisarNumeroCartaoEntaoRetorneBuscaComSucesso() throws Exception {
+        Cliente cliente = new Cliente(NOME_CLIENTE, CPF_CLIENTE, ENDERECO_CLIENTE);
+        Conta conta = new Conta(NUMERO_CONTA, cliente, SALDO_CONTA, IS_ATIVO_CONTA);
+        Cartao cartao = new Cartao(NUMERO_CARTAO, CVV_CARTAO, DT_VENCIMENTO_CARTAO, cliente, conta, IS_BLOQUEADO_CARTAO);
+
+        when(mockHashMapCartao.get(NUMERO_CARTAO)).thenReturn(cartao);
+
+        Cartao resultadoReal = cartaoServiceImpl.buscarCartaoPorNumero(NUMERO_CARTAO);
+
+        assertEquals(CPF_CLIENTE, resultadoReal.getCliente().getCpf());
+        assertEquals(NUMERO_CONTA, resultadoReal.getConta().getNumeroConta());
+        assertEquals(NUMERO_CARTAO, resultadoReal.getNumeroCartao());
+    }
+
     @ParameterizedTest
     @NullAndEmptySource //quando a função for executada passa null e depois vazia
     public void quandoCartaoPesquisarNumeroCartaoForVazioOuNuloEntaoExibaMensagem(String numeroCartao) {
@@ -103,6 +139,21 @@ public class CartaoServiceImplTest {
                 cartaoServiceImpl.buscarCartaoPorNumero(numeroCartao)
         );
         assertEquals("O cartão informado não foi encontrado. Cadastre-o e tente novamente.\n",
+                exception.getMessage());
+    }
+
+    @Test
+    public void quandoCartaoPesquisarNumeroCartaoVerifiqueSeCartaoEstaBloqueadoEntaoExibaMensagem() {
+        Cliente cliente = new Cliente(NOME_CLIENTE, CPF_CLIENTE, ENDERECO_CLIENTE);
+        Conta conta = new Conta(NUMERO_CONTA, cliente, SALDO_CONTA, IS_ATIVO_CONTA);
+        Cartao cartao = new Cartao(NUMERO_CARTAO, CVV_CARTAO, DT_VENCIMENTO_CARTAO, cliente, conta, true);
+
+        when(mockHashMapCartao.get(NUMERO_CARTAO)).thenReturn(cartao);
+
+        Exception exception = assertThrows(Exception.class, () ->
+                cartaoServiceImpl.buscarCartaoPorNumero(NUMERO_CARTAO)
+        );
+        assertEquals("Esse cartão está bloqueado.\n",
                 exception.getMessage());
     }
 }
