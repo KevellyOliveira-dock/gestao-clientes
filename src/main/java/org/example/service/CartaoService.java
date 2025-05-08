166def73b6b3dd34d19e689856a991860dd09eaa
Kevelly <kevelly.oliveira@dock.tech>
2025-05-08 08:27:09 -0300

    Criado: método para buscar cartão por CPF

--- src/main/java/org/example/service/CartaoService.java
+++ src/main/java/org/example/service/CartaoService.java
@@ -2,8 +2,12 @@ package org.example.service;
 
 import org.example.model.Cartao;
 
+import java.util.List;
+
 public interface CartaoService {
     Cartao cadastrarCartao(String cpf, String numeroConta) throws Exception;
 
     Cartao buscarCartaoPorNumero(String numeroCartao) throws Exception;
+
+    List<Cartao> buscarCartoesPorCPF(String cpf);
 }
