f366fa3f894471cac252f40120d70017d3d8671d
Kevelly <kevelly.oliveira@dock.tech>
2025-05-14 10:17:03 -0300

    Refatorado: método de executar com split

--- src/main/java/org/example/controller/FaturaController.java
+++ src/main/java/org/example/controller/FaturaController.java
@@ -1,17 +1,36 @@
 package org.example.controller;
 
+import org.example.service.FaturaService;
+
+import java.util.Scanner;
+
 public class FaturaController implements Controller {
+    private final Scanner scanner;
+    private final FaturaService faturaService;
+
+    public FaturaController(FaturaService faturaService, Scanner scanner) {
+        this.faturaService = faturaService;
+        this.scanner = scanner;
+    }
+
 
     public String executar(String comando) {
+        if (comando.equals("faturas")) {
+            return """
+                    -----------------------------
+                    | fechar {número do cartão} |
+                    | pagar {número do cartão}  |
+                    -----------------------------""";
+        }
 
-        switch (comando) {
-            case "faturas":
-                return """
-                        -----------------------------
-                        | fechar {número do cartão} |
-                        -----------------------------""";
+        String[] partes = comando.split(" ");
+        var acao = partes[1];
 
-            case "faturas fechar":
+        switch (acao) {
+            case "fechar":
+                return "não implementado";
+
+            case "pagar":
                 return "não implementado";
 
             default:
