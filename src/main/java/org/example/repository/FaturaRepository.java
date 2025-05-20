2c3b52332e2bae230d9d8263cc252ffb2994252c
Kevelly <kevelly.oliveira@dock.tech>
2025-05-20 11:30:23 -0300

    Criado: camada de persistência para fatura

---
+++ src/main/java/org/example/repository/FaturaRepository.java
@@ -0,0 +1,12 @@
+package org.example.repository;
+
+import org.example.model.Fatura;
+
+import java.util.Collection;
+
+
+public interface FaturaRepository {
+    Fatura cadastrar(Fatura fatura);
+
+    Collection<Fatura> buscarPorNumeroCartao(String numeroCartao);
+}
