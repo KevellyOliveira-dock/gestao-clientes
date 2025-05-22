981d35ca0288dbf39beef5c15a2b63488877db5e
Kevelly <kevelly.oliveira@dock.tech>
2025-05-22 09:43:54 -0300

    adicionado: Campo para identificação de pagamento da fatura

---
+++ src/main/java/org/example/model/Fatura.java
@@ -0,0 +1,17 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class Fatura {
+    private List<String> transacao;
+    private String dtVencimento;
+    private Cartao cartao;
+    private double valor;
+    private boolean isPago;
+
+}
